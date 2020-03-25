e45edbf4e40f2579d2d32bbe11460e15669ad85e
Robbin Law <robbinl@telusplanet.net>
2020-03-25 12:52:46 -0600

    a

--- WebFormsDemo/Site.Master.designer.cs
+++ WebFormsDemo/Site.Master.designer.cs
@@ -15,6 +15,15 @@ namespace WebFormsDemo
     {
 
         /// <summary>
+        /// navbarSubMenu control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlAnchor navbarSubMenu;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
